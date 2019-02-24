da87507da4aad371945612e18fa1319f0b5f017b
todoapam <todoapam@gmail.com>
2019-02-24 18:56:09 -0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php 
+function learwp_resources() {
+	wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'learwp_resources');
+
+?>
--- index.php
+++ index.php
@@ -5,15 +5,19 @@ get_header();
 if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
 
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<article class="post">
+
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php the_content(); ?>
+		</article>
 
 	<?php endwhile;
 
-	else : 
+else : 
 
-		echo '<p>No content found</p>';
+	echo '<p>No content found</p>';
 
-	endif;
+endif;
 
 get_footer();
 ?>
